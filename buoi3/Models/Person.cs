a146929294b7fa52576a59778a88993ead4a3913
Linh Nhi <tinhnhi2208@gmail.com>
2025-09-03 18:11:06 +0700

    BaiTapBuoi1,2,3

---
+++ buoi3/Models/Person.cs
@@ -0,0 +1,16 @@
+namespace buoi3.Models
+{
+    public class Person 
+    {
+        public string FullName { get; set ;}
+        public int Date { get; set; }
+        public int age 
+        {
+            get
+            {
+                int NamNay = DateTime.Now.Year;
+                return NamNay - Date;
+            }
+        }
+    }
+}
---
+++ buoi3/Models/Toan.cs
@@ -0,0 +1,10 @@
+namespace buoi3.Models
+{
+    public class Toan
+    {
+        public double so1{ get; set;}
+        public double so2{ get; set;}
+        public string phepToan { get; set;}
+        public double Ketqua { get; set;}
+    }
+}
